1161c74ad7c7a0e27e32d7d8eb4a44baf580d13f
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2022-10-02 17:06:04 +0900

    Update getNewestViewCount_v3_D.py

--- getNewestViewCount_v3_D.py
+++ getNewestViewCount_v3_D.py
@@ -104,7 +104,7 @@ def main():
         client = gspread.oauth();
         sheet = client.open_by_key(edit_fileid).sheet1
         current_rows = len(sheet.col_values(2));
-        hist = sheet.get("B"+str(current_rows))[0][0] if current_rows!=0 else 0;
+        hist = int(sheet.get("B"+str(current_rows))[0][0]) if current_rows!=0 else 0;
         
         if sheet.get("A1").first() == None:
             sheet.insert_row(["時刻", "再生数", "高評価数", "コメント数"]);
@@ -139,7 +139,7 @@ def main():
                 csv = ",".join(map(str, arr))+"\n";
                 f.write(csv);
 
-                sheet.append_row(arr);
+                sheet.append_row(arr, value_input_option='USER_ENTERED');
                 current_rows+= 1;
                 
                 for t, e in zip(txt, arr):
